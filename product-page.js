691ed14f8e8f20d2f26f90ee17696a9d66700aa1
ImpliedKaos <igmobrooks@gmail.com>
2026-03-25 16:17:53 -0500

    Update product-page.js

--- product-page.js
+++ product-page.js
@@ -59,10 +59,10 @@ function populate(record) {
   const f = record.fields || {};
   setText("materials", f.materials);
   setText("care", f.care);
-  setText("coo", f.COO);
-  setText("po", f.PO);
-  setText("style", f.Style);
-  setText("description", f.Description);
+  setText("coo", f.coo);
+  setText("po", f.po);
+  setText("style", f.style);
+  setText("description", f.description);
 
   const dump = document.getElementById("recordDump");
   if (dump) dump.textContent = JSON.stringify(record, null, 2);
